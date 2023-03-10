88b26d0f8a1755210ce74528313b9e5fd005e7a3
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-03-10 22:07:26 +0200

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,54 @@
+﻿
+BankAccount myAccount = new BankAccount();
+myAccount.BankName = "Deutsche Bank ";
+myAccount.BranchName = "Dresden";
+myAccount.BranchAddress = "142 Hantstrasses ";
+myAccount.AccountNumber = "DE01546";
+myAccount.Accountcurrancy = "USD";
+myAccount.Balance = 1500;
+
+Console.WriteLine(myAccount.BankName);
+Console.WriteLine(myAccount.Balance);
+
+myAccount.deposit(500);
+class BankAccount
+{
+    public string  BankName  { get; set; }
+    public string BranchName { get; set; }
+    public string BranchAddress  { get; set; }
+    public string AccountNumber  { get; set; }
+    public string Accountcurrancy  { get; set; }
+    public decimal Balance  { get; set; }
+
+    public void withdraw (decimal amount )
+    {
+        decimal newBalance = Balance - amount;
+        if (newBalance < 0)
+        {
+            Console.WriteLine("Invalid transaction ");
+        }
+        else
+        {
+            Console.WriteLine($"withdraw amount is {amount}");
+            Console.WriteLine($"your new balance for {AccountNumber} is {newBalance}");
+        }
+        Console.WriteLine($"Transaction date {DateTime.Now}");
+    }
+    public void deposit(decimal amount)
+    {
+        decimal newBalance = Balance + amount;
+        Console.WriteLine($"decimal amount is {amount}");
+        Console.WriteLine($"your new balance for {AccountNumber} is {newBalance}");
+        Console.WriteLine($"Transaction date {DateTime.Now}");
+    }
+    
+
+
+
+
+
+}
+
+
+
+
